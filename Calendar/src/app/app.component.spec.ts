38200776bda88c801f2fe21e6d95417c7ed707e6
kiev sahara <alex.jackfish@gmail.com>
2020-05-30 23:04:24 +0300

    ..

--- Calendar/src/app/app.component.spec.ts
+++ Calendar/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Calendar'`, () => {
+  it(`should have as title 'calendar'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Calendar');
+    expect(app.title).toEqual('calendar');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Calendar app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('calendar app is running!');
   });
 });
